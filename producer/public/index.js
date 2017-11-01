ab02d9ef39af9d467ea06ee7f603af0792609942
Calvin <calvinjhfeng@gmail.com>
2017-10-31 17:41:51 -0700

    Looks like the kafka producer is working

--- producer/public/index.js
+++ producer/public/index.js
@@ -1,1 +1,15 @@
 console.log('Hello');
+
+const socket = io('http://localhost:8000');
+
+socket.on('connect', () => {
+  console.log('Connected');
+});
+
+socket.on('message', (msg) => {
+  console.log(msg);
+})
+
+setInterval(() => {
+  socket.send("Hello world!!!");
+}, 1000)
--- producer/server.js
+++ producer/server.js
@@ -1,23 +1,62 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const websocket = require('express-ws')(app, server);
+
+const httpServer = require('http').createServer(app);
+
+const Kafka = require('kafka-node');
+const Producer = Kafka.Producer;
+const Client = Kafka.Client;
+
+const myClient = new Client('localhost:2181');
+const myProducer = new Producer(myClient);
+
+myProducer.on('ready', () => {
+  const shouldAsync = false;
+  myProducer.createTopics(['messages'], shouldAsync, (err, data) => {
+    console.log(err, data);
+  });
+})
+
+const connectedClient = {};
 
 // Routers
-
 app.use(express.static('public'))
 
 app.get('/', (req, res, next) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.ws('/', (ws, req) => {
-    ws.on('message', (msg) => {
-        console.log('Incoming message:', msg)
-        ws.send("Your message is received");
-        connectedClient.forEach((client) => {
-            client.send(msg);
-        });
-    });
+const io = require('socket.io')(httpServer);
+
+io.on('connection', function(socket){
+  console.log('a user connected', socket.id);
+  
+  socket.on('disconnect', () => {
+    console.log(socket.id, 'has disconnected');
+  });
+  
+  socket.on('message', createMessageHandler(socket));
+  
+  connectedClient[socket] = socket;
 });
-server.listen(8000);
+
+function createMessageHandler(socket) {
+  return (msg) => {
+    console.log(msg, 'from', socket.id)
+    io.emit('message', msg);
+    
+    const payload = {
+      topic: 'message',
+      messages: msg, // multi messages should be a array, single message can be just a string or a KeyedMessage instance
+      timestamp: Date.now() // <-- defaults to Date.now() (only available with kafka v0.10 and KafkaClient only)
+    }
+    
+    myProducer.send([payload], (err, data) => {
+      console.log(err, data, "is committed to Kafka cluster");
+    })
+  }
+}
+
+httpServer.listen(8000, () => {
+  console.log("Serving and listening on 8000")
+});
